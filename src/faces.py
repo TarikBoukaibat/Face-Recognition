8c623705597262f08f433297b2e482e50b6bd267
Tarik Boukaibat <tarikboukaibat01@gmail.com>
2022-06-28 18:26:59 +0100

    Add project

---
+++ src/faces.py
@@ -0,0 +1,193 @@
+import numpy as np
+import cv2
+import pickle
+padding=20
+def faceBox(faceNet,frame):
+    frameHeight=frame.shape[0]
+    frameWidth=frame.shape[1]
+    blob=cv2.dnn.blobFromImage(frame, 1.0, (300,300), [104,117,123], swapRB=False)
+    faceNet.setInput(blob)
+    detection=faceNet.forward()
+    bboxs=[]
+    for i in range(detection.shape[2]):
+        confidence=detection[0,0,i,2]
+        if confidence>0.7:
+            x1=int(detection[0,0,i,3]*frameWidth)
+            y1=int(detection[0,0,i,4]*frameHeight)
+            x2=int(detection[0,0,i,5]*frameWidth)
+            y2=int(detection[0,0,i,6]*frameHeight)
+            bboxs.append([x1,y1,x2,y2])
+            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0), 1)
+    return frame, bboxs
+
+
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+ageProto = "age_deploy.prototxt"
+ageModel = "age_net.caffemodel"
+
+genderProto = "gender_deploy.prototxt"
+genderModel = "gender_net.caffemodel"
+
+
+
+faceNet=cv2.dnn.readNet(faceModel, faceProto)
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList = ['Male', 'Female']
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("./recognizers/face-trainner.yml")
+
+labels = {"person_name": 1}
+with open("pickles/face-labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    #///////
+    frame, bboxs = faceBox(faceNet, frame)
+    for bbox in bboxs:
+        # face=frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+        face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
+               max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
+        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPred = genderNet.forward()
+        gender = genderList[genderPred[0].argmax()]
+
+        ageNet.setInput(blob)
+        agePred = ageNet.forward()
+        age = ageList[agePred[0].argmax()]
+
+        label = "{},{}".format(gender, age)
+        cv2.rectangle(frame, (bbox[0], bbox[1] - 30), (bbox[2], bbox[1]), (0, 255, 0), -1)
+        cv2.putText(frame, label, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+    #///////
+
+
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+    	#print(x,y,w,h)
+    	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+    	roi_color = frame[y:y+h, x:x+w]
+
+    	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
+    	id_, conf = recognizer.predict(roi_gray)
+    	if conf>=4 and conf <= 85:
+    		#print(5: #id_)
+    		#print(labels[id_])
+    		font = cv2.FONT_HERSHEY_SIMPLEX
+    		name = labels[id_]
+    		color = (255, 255, 255)
+    		stroke = 2
+    		cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+    	img_item = "7.png"
+    	cv2.imwrite(img_item, roi_color)
+
+    	color = (255, 0, 0) #BGR 0-255 
+    	stroke = 2
+    	end_cord_x = x + w
+    	end_cord_y = y + h
+    	cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    	#subitems = smile_cascade.detectMultiScale(roi_gray)
+    	#for (ex,ey,ew,eh) in subitems:
+    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+#///////////////////////////////////////////////////////////
+
+def faceBox(faceNet,frame):
+    frameHeight=frame.shape[0]
+    frameWidth=frame.shape[1]
+    blob=cv2.dnn.blobFromImage(frame, 1.0, (300,300), [104,117,123], swapRB=False)
+    faceNet.setInput(blob)
+    detection=faceNet.forward()
+    bboxs=[]
+    for i in range(detection.shape[2]):
+        confidence=detection[0,0,i,2]
+        if confidence>0.7:
+            x1=int(detection[0,0,i,3]*frameWidth)
+            y1=int(detection[0,0,i,4]*frameHeight)
+            x2=int(detection[0,0,i,5]*frameWidth)
+            y2=int(detection[0,0,i,6]*frameHeight)
+            bboxs.append([x1,y1,x2,y2])
+            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0), 1)
+    return frame, bboxs
+
+
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+ageProto = "age_deploy.prototxt"
+ageModel = "age_net.caffemodel"
+
+genderProto = "gender_deploy.prototxt"
+genderModel = "gender_net.caffemodel"
+
+
+
+faceNet=cv2.dnn.readNet(faceModel, faceProto)
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList = ['Male', 'Female']
+
+
+video=cv2.VideoCapture(0)
+
+padding=20
+
+while True:
+    ret,frame=video.read()
+    frame,bboxs=faceBox(faceNet,frame)
+    for bbox in bboxs:
+        # face=frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPred=genderNet.forward()
+        gender=genderList[genderPred[0].argmax()]
+
+
+        ageNet.setInput(blob)
+        agePred=ageNet.forward()
+        age=ageList[agePred[0].argmax()]
+
+
+        label="{},{}".format(gender,age)
+        cv2.rectangle(frame,(bbox[0], bbox[1]-30), (bbox[2], bbox[1]), (0,255,0),-1)
+        cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2,cv2.LINE_AA)
+    cv2.imshow("Age-Gender",frame)
+    k=cv2.waitKey(1)
+    if k==ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
